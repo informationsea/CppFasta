1d08208544303967ffee500e1a2c6b4b0dc23eea
OKAMURA, Yasunobu <okamura@informationsea.info>
2015-12-04 18:51:33 +0900

    add alternative read mode for paired end file

--- src/cppfasta2.hpp
+++ src/cppfasta2.hpp
@@ -5,7 +5,7 @@
 #include <string.h>
 
 #ifndef CPPFASTA_MAXIMUM_SEQUENCE_LENGTH
-#define CPPFASTA_MAXIMUM_SEQUENCE_LENGTH 200
+#define CPPFASTA_MAXIMUM_SEQUENCE_LENGTH 500
 #endif
 
 namespace cppfasta {
@@ -31,8 +31,10 @@ namespace cppfasta {
     class FastqReader2
     {
     public:
-        FastqReader2(FILE *file) {
-            m_file = file;
+        FastqReader2(FILE *file) : m_file(file), m_file2(0), m_fileIndex(0) {
+        }
+
+        FastqReader2(FILE *file, FILE *file2) : m_file(file), m_file2(file2), m_fileIndex(0) {
         }
         
         virtual ~FastqReader2() {
@@ -40,9 +42,20 @@ namespace cppfasta {
         }
 
         bool next(struct SequenceRecord2 *record) {
+            FILE *current;
+
+            if (m_file2 && m_fileIndex == 1) {
+                current = m_file2;
+                m_fileIndex = 0;
+            } else {
+                current = m_file;
+                m_fileIndex = 1;
+            }
+            
             char *result;
 
-            result = fgets(dummy, CPPFASTA_MAXIMUM_SEQUENCE_LENGTH-1, m_file);
+            result = fgets(dummy, CPPFASTA_MAXIMUM_SEQUENCE_LENGTH-1, current);
+            //fprintf(stderr, "LINE0: %s\n", result);
             if (!clean(result)) goto onerror;
             if (dummy[0] != '@') {
                 fprintf(stderr, "Input file is not FASTQ file\n");
@@ -51,13 +64,16 @@ namespace cppfasta {
             strncpy(record->name, result+1, CPPFASTA_MAXIMUM_SEQUENCE_LENGTH-1); // skip @
             record->name[CPPFASTA_MAXIMUM_SEQUENCE_LENGTH-1] = '\0'; //terminate
             
-            result = fgets(record->sequence, CPPFASTA_MAXIMUM_SEQUENCE_LENGTH-1, m_file);
+            result = fgets(record->sequence, CPPFASTA_MAXIMUM_SEQUENCE_LENGTH-1, current);
+            //fprintf(stderr, "LINE1: %s\n", result);
             if (!clean(result)) goto onerror;
             
-            result = fgets(dummy, CPPFASTA_MAXIMUM_SEQUENCE_LENGTH-1, m_file);
+            result = fgets(dummy, CPPFASTA_MAXIMUM_SEQUENCE_LENGTH-1, current);
+            //fprintf(stderr, "LINE2: %s\n", result);
             if (!clean(result)) goto onerror;
             
-            result = fgets(record->quality, CPPFASTA_MAXIMUM_SEQUENCE_LENGTH-1, m_file);
+            result = fgets(record->quality, CPPFASTA_MAXIMUM_SEQUENCE_LENGTH-1, current);
+            //fprintf(stderr, "LINE3: %s\n", result);
             if (!clean(result)) goto onerror;
 
             return true;
@@ -66,13 +82,15 @@ namespace cppfasta {
                 if (!feof(m_file))
                     perror("Cannot read FASTQ file");
             } else {
-                fprintf(stderr, "Too long line. Please consider increase CPPFASTA_MAXIMUM_SEQUENCE_LENGTH in cppfasta2.hpp\n");
+                fprintf(stderr, "Too long line. Please consider to increase CPPFASTA_MAXIMUM_SEQUENCE_LENGTH in cppfasta2.hpp\n");
             }
             return false;
         }
         
     private:
         FILE *m_file;
+        FILE *m_file2;
+        int m_fileIndex;
         char dummy[CPPFASTA_MAXIMUM_SEQUENCE_LENGTH];
         
         inline bool clean(char *line) {
--- test/test_fasta2.cpp
+++ test/test_fasta2.cpp
@@ -3,13 +3,20 @@
 
 using namespace cppfasta;
 
-void test_fastqEntry(FastqReader2 *reader, const char *name, const char *seq, const char* quality) {
+void test_fastqEntry2(FastqReader2 *reader, const char *name, const char *seq, const char *quality) {
     SequenceRecord2 record;
     ASSERT_TRUE(reader->next(&record));
     ASSERT_STREQ(name, record.name);
     ASSERT_STREQ(seq, record.sequence);
     ASSERT_STREQ(quality, record.quality);
 }
+
+#define test_fastqEntry(reader, name, seq, quality) \
+    do { \
+        test_fastqEntry2(reader, name, seq, quality);\
+        if (HasFatalFailure())\
+            return;\
+    } while(0);
 
 TEST(FASTX_READER, FASTQ) {
     //system("gzip -dk ./data/DRR000035_head100.fastq.gz");
@@ -31,6 +38,33 @@ TEST(FASTX_READER, FASTQ) {
 
     SequenceRecord2 record;
     for (int i = 0; i < 100/4-3; i++) {
+        ASSERT_TRUE(reader.next(&record));
+    }
+
+    ASSERT_FALSE(reader.next(&record));
+}
+
+TEST(FASTX_READER, FASTQ2) {
+    //system("gzip -dk ./data/DRR000035_head100.fastq.gz");
+    FILE *file = fopen("./data/DRR000035_head100.fastq", "r");
+    FILE *file2 = fopen("./data/DRR000035_101-200.fastq", "r");
+    FastqReader2 reader(file, file2);
+
+    test_fastqEntry(&reader,
+                    "DRR000035.1 20E09AAXX:1:1:107:276 length=36",
+                    "TCACAATGTATACTCTCGAGATCCTAATTATCTGAA",
+                    "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+    test_fastqEntry(&reader,
+                    "DRR000035.27 20E09AAXX:1:1:88:355 length=36",
+                    "CACAATTAAAGCTAAAATAATATAAAAATAATTCGA",
+                    "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+    test_fastqEntry(&reader,
+                    "DRR000035.2 20E09AAXX:1:1:106:201 length=36",
+                    "TATTTTTATTTAAGAGAATTTTTTAAAGCACTCTGG",
+                    "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+
+    SequenceRecord2 record;
+    for (int i = 0; i < 200/4-3; i++) {
         ASSERT_TRUE(reader.next(&record));
     }
 
